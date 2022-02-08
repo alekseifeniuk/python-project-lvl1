af2654b3ec2b654048c5d5f431c6a2a2ceafa969
Aleksei <Ffl.B-Unit@mail.ru>
2022-02-08 23:19:59 +0300

    Step_7: added brain-gcd game

---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,12 @@
+#!usr/bin/env python3
+
+from brain_games.games import brain_gcd_game
+from brain_games.game_core import game_core
+
+
+def main():
+    game_core(brain_gcd_game)
+
+
+if __name__ == "__main__":
+    main()
